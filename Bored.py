a4d39864d13d6ec25984efdbf2f2a402fbe95a69
thebookcheese <jasperthebook@gmail.com>
2024-06-14 15:02:08 +0100

    Added computer logic

--- Bored.py
+++ Bored.py
@@ -31,17 +31,45 @@ def Blackjack():
             exit()
     else:
         pass
-    while (Card1 + Card2) is not 21 or (Card1 + Card2) < 21:
+    print("You got ",Card1," and ",Card2)
+    while (Card1 + Card2) != 21 or (Card1 + Card2) < 21 or (AiCard1 + AiCard2) != 21 or (AiCard1 + AiCard2) < 21:
         if (Card1 + Card2) > 21:
             print("Bust, you lose.")
             exit()
         elif (Card1 + Card2) < 21:
-            Pick = input("Do you want to twist (1) or stick (2)")
+            Pick = int(input("Do you want to twist (1) or stick (2)"))
             if Pick == 1:
                 Card3 = random.randint(1,10)
+                if (Card1 + Card2 + Card3) > 21:
+                    print("Bust, you lose")
+                    exit()
+                elif (Card1 + Card2 + Card3) == 21:
+                    print("You win")
+                    exit()
+                else:
+                    print("You got ",Card3)
+                    Pick = 2
             elif Pick == 2:
                 AiCard1 = random.randint(1,11)
                 AiCard2 = random.randint(1,11)
+                if AiCard1 == 11:
+                    if (AiCard2 + 1) == 21:
+                        AiCard1 = 1
+                    elif (AiCard2 + 10) == 21:
+                        AiCard1 = 10
+                    else:
+                        AiCard1 = 10
+                else:
+                    pass
+                if AiCard2 == 11:
+                    if (AiCard1 + 1) == 21:
+                        AiCard2 = 1
+                    elif (AiCard1 + 10) == 21:
+                        AiCard2 = 10
+                    else:
+                        AiCard2 = 10
+                else:
+                    pass
                 if (AiCard1 + AiCard2) > 21:
                     print("You win!")
                     exit()
@@ -52,8 +80,11 @@ def Blackjack():
 
 
 
-Run = input("Software run? \n Dice Thing (1) \n")
+Run = int(input("Software run? \n Dice Thing (1) \n Blackjack (2) \n"))
 if Run == 1:
     Dice()
+elif Run == 2:
+    Blackjack()
 else:
-    pass
+    print("Go away then")
+    exit()
