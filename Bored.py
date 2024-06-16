96ace536ab7612eff41e738538e7559fb72fcdb9
thebookcheese <jasperthebook@gmail.com>
2024-06-16 20:34:01 +0100

    Fixed Blackjack Logic

--- Bored.py
+++ Bored.py
@@ -13,6 +13,7 @@ def Dice():
 def Blackjack():
     Card1 = random.randint(1,11)
     Card2 = random.randint(1,11)
+    Card3 = 0
     if Card1 == 11:
         Ace = input("Should it be a 1 or 10 \n Type 1 if 1 \n Type 2 if 10")        
         if Ace == 1:
@@ -32,11 +33,11 @@ def Blackjack():
     else:
         pass
     print("You got ",Card1," and ",Card2)
-    while (Card1 + Card2) != 21 or (Card1 + Card2) < 21 or (AiCard1 + AiCard2) != 21 or (AiCard1 + AiCard2) < 21:
-        if (Card1 + Card2) > 21:
+    while (Card1 + Card2 + Card3) != 21 or (Card1 + Card2 + Card3) < 21 or (AiCard1 + AiCard2 + AiCard3) != 21 or (AiCard1 + AiCard2 + AiCard3) < 21:
+        if (Card1 + Card2 + Card3) > 21:
             print("Bust, you lose.")
             exit()
-        elif (Card1 + Card2) < 21:
+        elif (Card1 + Card2 + Card3) < 21:
             Pick = int(input("Do you want to twist (1) or stick (2)"))
             if Pick == 1:
                 Card3 = random.randint(1,10)
@@ -52,6 +53,7 @@ def Blackjack():
             elif Pick == 2:
                 AiCard1 = random.randint(1,11)
                 AiCard2 = random.randint(1,11)
+                AiCard3 = 0
                 if AiCard1 == 11:
                     if (AiCard2 + 1) == 21:
                         AiCard1 = 1
@@ -70,13 +72,26 @@ def Blackjack():
                         AiCard2 = 10
                 else:
                     pass
-                if (AiCard1 + AiCard2) > 21:
+                if (AiCard1 + AiCard2 + AiCard3) > 21:
                     print("You win!")
                     exit()
-                elif (AiCard1 + AiCard2) == 21:
+                elif (AiCard1 + AiCard2 + AiCard3) == 21:
                     print("Ai Won, you lost")
                 else:
                     pass
+                if (21 - (AiCard1 + AiCard2 + AiCard3)) > 10 and (21 - (AiCard1 + AiCard2 + AiCard3)) < 15:
+                    AiCard3 = random.randint(1,10)
+                    if (AiCard1 + AiCard2 + AiCard3) == 21:
+                        print("Ai wins, you lose")
+                        exit()
+                    elif (AiCard1 + AiCard2 + AiCard3) > 21:
+                        print("Ai bust, you win!")
+                    else:
+                        pass
+                else:
+                    print("Ai stood")
+
+
 
 
 
